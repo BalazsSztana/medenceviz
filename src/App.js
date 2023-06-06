3a75fc524e59dc38832006e6f033edb670b660b3
Sztana Balázs <balazssztana@outlook.com>
2023-06-06 18:01:00 +0200

    JavaScript elkészítve

---
+++ src/App.js
@@ -0,0 +1,51 @@
+const doc = {
+    heightInput: document.querySelector('#height'),
+    widthInput: document.querySelector('#width'),
+    lengthInput: document.querySelector('#lenght'),
+    volumeInput: document.querySelector('#volume'),
+    calcButton: document.querySelector('#calcButton')
+};
+
+const state = {
+    index: 0
+};
+
+window.addEventListener('load', () => {
+    init();
+});
+
+function init() {
+    if(doc.calcButton) {
+        doc.calcButton.addEventListener('click', () => {
+            console.log('működik')
+            startCalc();
+        });
+    }
+    // checkInput(a);
+}
+
+function startCalc() {
+    let height = doc.heightInput.value;
+    let width = doc.widthInput.value;
+    let length = doc.lengthInput.value;
+    state.volume = calcBodyIndex(height, width, length);
+    doc.volumeInput.value = state.volume;
+}
+
+function calcBodyIndex(height, width, length) {
+    return ((height-10) * width * length)/1000000;
+}
+
+function checkInput(input) {
+    let inputStr = String(input);
+    let inputNum = Number(input);
+    if(inputStr.match(/^[0-9.]+$/)){
+        if(inputNum > 0) {
+            return true;
+        }else {
+            return false;
+        }        
+    }else{
+        return false;
+    }    
+}
